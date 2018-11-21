60d9375813940f7b8ebe7720eefe663ebd3c970e
ntsikinonjo <ntsikinonjo@gmail.com>
2018-11-21 15:16:19 +0200

    pop up menu

--- main.js
+++ main.js
@@ -15,6 +15,13 @@ var xWin, yWin
 var connection
 
 /**
+ * element variables
+ * 
+ * elements that are found in the DOM
+ */
+var menuStroke = document.getElementById('menu-stroke')
+
+/**
  * DOM elements are finished loading
  */
 $(document).ready(function() {
@@ -36,4 +43,28 @@ $(window).on('resize', function() {
     // get window width and window height
     xWin = $(window).width()
     yWin = $(window).height()
+})
+
+/**
+ * Events
+ */
+menuStroke.addEventListener('mouseover', function() {
+
+    // change stroke colours
+    $('.menu-btn-stroke').css('background', '#3771C8')
+})
+menuStroke.addEventListener('mouseleave', function() {
+
+    // change stroke colours back
+    $('.menu-btn-stroke').css('background', '#202020')
+})
+menuStroke.addEventListener('click', function(event) {
+
+    // prevent default before dealing with click
+    event.preventDefault()
+
+    // change stroke colours
+    $('.menu-btn-stroke').css('background', '#3771C8')
+
+    // handle click
 })
